36e7d2040e330e5d94c3ebd49108a17fcfb649be
MrSleepyRags <108429016+MrSleepyRags@users.noreply.github.com>
2022-07-01 20:02:23 -0400

    Added 1000hz polling, changed lock combo

--- keyboards/novelkeys/nk87/keymaps/vial/config.h
+++ keyboards/novelkeys/nk87/keymaps/vial/config.h
@@ -3,8 +3,11 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0x9C, 0x04, 0x30, 0x89, 0xCC, 0x5A, 0xBD, 0x5A}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
 
 #define DEBOUNCE 5
 #define TAPPING_TERM 200
+
+#define USB_POLLING_INTERVAL_MS 1
+#define QMK_KEYS_PER_SCAN 4
